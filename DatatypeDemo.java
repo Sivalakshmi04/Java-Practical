fb18e28d8b98b50284c1529c75a58ba06898f0e4
Sivalakshmi04 <79317708+Sivalakshmi04@users.noreply.github.com>
2022-12-03 12:24:08 +0530

    Add files via upload

---
+++ DatatypeDemo.java
@@ -0,0 +1,43 @@
+package basic;
+public class DatatypeDemo {
+public static void main(String[] args) {
+//boolean example
+boolean IoT = true;
+ System.out.println(IoT);
+ 
+ //byte example
+ byte range;
+ range = 125;
+System.out.println(range);
+ 
+ //short example
+ short range1;
+ range1 = -128;
+ System.out.println(range1);
+ 
+ //int example
+ int range2 = 300000000;
+ System.out.println(range2);
+ 
+ //long example
+ long range3 = 30000000000L;
+ System.out.println(range3);
+ 
+ //double example
+ double number = 99.9;
+System.out.println(number);
+ 
+ //float example
+ float number1 = 99.9f;
+ System.out.println(number1);
+ 
+ //char example (Unicode value of A is '\u0041')
+ char letter = '\u0041';
+ System.out.println(letter);
+ char letter2 = 65; // ASCII value of 'A' is 65
+ System.out.println(letter2);
+ char letter3 = 'A';
+ System.out.println(letter3);
+ 
+}
+}
